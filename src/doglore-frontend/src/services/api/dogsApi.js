b17c17dd386571e63232e6902d5456ecab6d6bc4
Bogdan_Boyk0 <104592168+BogdanFromUkraine@users.noreply.github.com>
2026-05-12 19:11:23 +0300

    feat: add dog registration and createDocument methods

--- src/doglore-frontend/src/services/api/dogsApi.js
+++ src/doglore-frontend/src/services/api/dogsApi.js
@@ -1,6 +1,6 @@
 import { db } from "../firebase.js";
 import { doc, getDoc } from "firebase/firestore";
-import { getSubcollectionData } from "../dbService.js";
+import { getSubcollectionData, createDocument, createSubdocument } from "../dbService.js";
 
 // Отримати дані однієї собаки за ID + дані її породи
 export const fetchDogFullProfile = async (dogId) => {
@@ -37,3 +37,48 @@ export const fetchDogJournal = (dogId) => {
     // Тут ми звертаємось до підколекції training_logs щоб отримати данні із щоденника собаки
     return getSubcollectionData(`dogs/${dogId}/training_logs`);
 };
+
+// приклад, як треба передаавти данні у функцію
+//const newDog = {
+//    dogName: "укцк",
+//    breed_id: "ID_ПОРОДИ_З_ЕНЦИКЛОПЕДІЇ", // Обов'язково ID документа з колекції breeds
+//    gender: "Male",
+//    chip: "123-456-789",
+//    nutrition: "Natural food",
+//    trainingNotes: "", // Поки що порожньо
+//};
+
+export const registerDog = async (dogData) => {
+    try {
+        // Перевіряємо, чи передали id породи, щоб не було порожніх профілів
+        if (!dogData.breed_id) throw new Error("Необхідно вказати ID породи");
+
+        const newDogId = await createDocument("dogs", dogData);
+        console.log("Собаку успішно зареєстровано з ID:", newDogId);
+        return newDogId;
+    } catch (error) {
+        console.error("Помилка при реєстрації собаки:", error);
+        throw error;
+    }
+};
+
+//const healthData = {
+//    weight: 42.5,
+//    status: "Чудовий",
+//    lastCheckup: "Вакцинація пройшла успішно"
+//};
+
+// Додавання запису в Health Log
+export const addHealthLog = (dogId, healthData) =>
+    createSubdocument(`dogs/${dogId}/health_logs`, healthData);
+
+
+//const trainingData = {
+//    command: "Сидіти",
+//    mastery: 85,
+//    notes: "Добре реагує на ласощі"
+//};
+
+// Додавання запису в Training Log
+export const addTrainingLog = (dogId, trainingData) =>
+    createSubdocument(`dogs/${dogId}/training_logs`, trainingData);
